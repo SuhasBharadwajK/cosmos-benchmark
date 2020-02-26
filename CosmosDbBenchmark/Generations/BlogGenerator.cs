6746f75cbf2d82bc265973f6e0445982e147e560
Suhas Bharadwaj <suhas.b@technovert.com>
2020-02-26 18:43:23 +0530

    Implemented generation of blog using given parameters

--- CosmosDbBenchmark/Generations/BlogGenerator.cs
+++ CosmosDbBenchmark/Generations/BlogGenerator.cs
@@ -1,13 +1,69 @@
-namespace CosmosDbBenchmark
+using CosmosDbBenchmark.Models;
+using System.Collections.Generic;
+
+namespace CosmosDbBenchmark.Generations
 {
     public class BlogGenerator
     {
-        public BlogGenerator()
+        private int blogCount;
+
+        private int commentsPerBlog;
+
+        private int initialBlogSizeInKilobytes;
+
+        private int blogSizeMultiplier;
+
+        private int initialCommentSizeInBytes;
+
+        private int commentSizeMultiplier;
+
+        private int numberOfMultiplications;
+
+        public BlogGenerator(
+            int blogCount,
+            int commentsPerBlog,
+            int initialBlogSizeInKilobytes,
+            int blogSizeMultiplier,
+            int intialCommentSizeInBytes,
+            int commentSizeMultiplier,
+            int numberOfMultiplications)
         {
+            this.blogCount = blogCount;
+            this.initialCommentSizeInBytes = intialCommentSizeInBytes;
+            this.commentsPerBlog = commentsPerBlog;
+            this.commentSizeMultiplier = commentSizeMultiplier;
+            this.initialBlogSizeInKilobytes = initialBlogSizeInKilobytes;
+            this.blogSizeMultiplier = blogSizeMultiplier;
+            this.numberOfMultiplications = numberOfMultiplications;
         }
 
-        public void GenerateBlogsWithComments()
+        public List<BlogGenerationResult> GenerateBlogsWithComments()
         {
+            var result = new List<BlogGenerationResult>();
+            var blogSize = initialBlogSizeInKilobytes;
+            var commentSize = initialCommentSizeInBytes;
+
+            for (int i = 1; i <= numberOfMultiplications; i++)
+            {
+                for (int j = 0; j < blogCount; j++)
+                {
+                    var blogText = TextHelper.GetBytesOfString(blogSize, 1000);
+                    var comments = TextHelper.GetStringsOfLength(commentsPerBlog, commentSize);
+
+                    result.Add(new BlogGenerationResult
+                    {
+                        BlogSizeInKilobytes = blogSize,
+                        CommentSizeInBytes = commentSize,
+                        BlogText = blogText,
+                        Comments = comments
+                    });
+                }
+
+                blogSize *= blogSizeMultiplier;
+                commentSize *= commentSizeMultiplier;
+            }
+
+            return result;
         }
     }
 }
---
+++ CosmosDbBenchmark/Models/BlogGenerationResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CosmosDbBenchmark.Models
+{
+    public class BlogGenerationResult
+    {
+        public int BlogSizeInKilobytes { get; set; }
+
+        public int CommentSizeInBytes { get; set; }
+
+        public string BlogText { get; set; }
+
+        public List<string> Comments { get; set; }
+    }
+}
