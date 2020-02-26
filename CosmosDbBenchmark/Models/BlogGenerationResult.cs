7ef1462acdc99d4ded636f1ffc14cc31bfd4b8ba
Suhas Bharadwaj <suhas.b@technovert.com>
2020-02-26 21:09:06 +0530

    Added blog type to blog generation result

--- CosmosDbBenchmark/Models/BlogGenerationResult.cs
+++ CosmosDbBenchmark/Models/BlogGenerationResult.cs
@@ -10,6 +10,8 @@ namespace CosmosDbBenchmark.Models
 
         public string BlogText { get; set; }
 
+        public BlogType BlogType { get; set; }
+
         public List<string> Comments { get; set; }
     }
 }
