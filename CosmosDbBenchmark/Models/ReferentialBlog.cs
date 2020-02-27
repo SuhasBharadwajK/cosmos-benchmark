ffbd40aa60ba39643d6cc2eba49eb071862b8497
Suhas Bharadwaj <suhas.b@technovert.com>
2020-02-27 20:39:34 +0530

    Removed unused imports

--- CosmosDbBenchmark/Models/ReferentialBlog.cs
+++ CosmosDbBenchmark/Models/ReferentialBlog.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CosmosDbBenchmark.Models
+﻿namespace CosmosDbBenchmark.Models
 {
     public class ReferentialBlog : Blog
     {
