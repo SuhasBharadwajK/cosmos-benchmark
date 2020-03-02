f448d95322b4d19d06634c8fd3e109c12f2f55bb
Suhas Bharadwaj <suhas@keka.com>
2020-03-02 21:51:58 +0530

    Implemented getting of random benchmark results

--- CosmosDbBenchmark/Models/Benchmark.cs
+++ CosmosDbBenchmark/Models/Benchmark.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CosmosDbBenchmark.Models
 {
@@ -17,6 +19,22 @@ namespace CosmosDbBenchmark.Models
         public void AddResult(BenchmarkResult benchmarkResult)
         {
             this.BenchmarkResults.Add(benchmarkResult);
+        }
+
+        public BenchmarkResult GetRandomBenchmarkResult(BlogType blogType)
+        {
+            Random rnd = new Random();
+
+            if (blogType == BlogType.Embedded)
+            {
+                var embeddedResponses = this.BenchmarkResults.Where(r => r.EmbeddedBlogResponse != null).ToList();
+                return embeddedResponses[rnd.Next(embeddedResponses.Count)];
+            }
+            else
+            {
+                var referentialResponses = this.BenchmarkResults.Where(r => r.ReferentialBlogResponse != null).ToList();
+                return referentialResponses[rnd.Next(referentialResponses.Count)];
+            }
         }
     }
 }
