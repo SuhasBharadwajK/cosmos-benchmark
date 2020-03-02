ad48c4613e237edd20c9ed50eb2341f4d8322b13
Naveen Dharnamoni <dharnamoninaveen@gmail.com>
2020-03-02 16:47:04 +0530

    Revert "added csv generator for benchmark"
    This reverts commit b1b1391d6d175fb93cf69b8f4233c6fe476278ab.

--- CosmosDbBenchmark/Program.cs
+++ CosmosDbBenchmark/Program.cs
@@ -13,52 +13,29 @@ namespace CosmosDbBenchmark
                 CosmosBenchmarkRunner cosmosBenchmarkRunner = CosmosBenchmarkRunner.Initialize(2, 5, 10, 1, 100, 1, 5);
                 Console.WriteLine("Intialized Cosmos Benchmark Runner");
                 cosmosBenchmarkRunner.DeleteAllItems().GetAwaiter().GetResult();
-                CsvGenerator csvGenerator = new CsvGenerator();
-               
                 Console.WriteLine("Running Benchmark for creating blogs...");
                 var benchmarkCreateResults = cosmosBenchmarkRunner.BenchmarkCreatingBlogs().GetAwaiter().GetResult();
-                int blogSize = benchmarkCreateResults.BenchmarkResults.First().BlogGenerationResult.BlogSizeInKilobytes;
-                int commentSize = benchmarkCreateResults.BenchmarkResults.First().BlogGenerationResult.CommentSizeInBytes;
-                csvGenerator.GenerateBenchmarkCSV(benchmarkCreateResults, blogSize, commentSize);
-               
                 Console.WriteLine("Running Benchmark for updating blogs...");
                 var benchmarkUpdateResults = cosmosBenchmarkRunner.BenchmarkUpdatingBlogs().GetAwaiter().GetResult();
-                csvGenerator.GenerateBenchmarkCSV(benchmarkUpdateResults, blogSize, commentSize);
-                
                 Console.WriteLine("Running Benchmark for Get All Blogs Without Comments...");
                 var benchmarkGetBlogsWithoutComments = cosmosBenchmarkRunner.BenchmarkGettingAllBlogsWithoutComments().GetAwaiter().GetResult();
-                csvGenerator.GenerateBenchmarkCSV(benchmarkGetBlogsWithoutComments, blogSize, commentSize);
-
                 Console.WriteLine("Running Benchmark for Get All Blogs With Comments...");
                 var benchmarkGetBlogsWithComments = cosmosBenchmarkRunner.BenchmarkGettingAllBlogsWithAllComments().GetAwaiter().GetResult();
-                csvGenerator.GenerateBenchmarkCSV(benchmarkGetBlogsWithComments, blogSize, commentSize);
                 var embeddedId = benchmarkGetBlogsWithComments.BenchmarkResults.First().EmbeddedBlogResponse.Item.Id;
                 var referenceId = benchmarkGetBlogsWithComments.BenchmarkResults.Last().ReferentialBlogResponse.Item.Id;
-                
                 Console.WriteLine("Running Benchmark for Getting a Blog...");
-                var benchmarkGetABlog = cosmosBenchmarkRunner.BenchmarkGettingBlog(referenceId, embeddedId).GetAwaiter().GetResult();
-                csvGenerator.GenerateBenchmarkCSV(benchmarkGetABlog, blogSize, commentSize);
-
+                var benchmarkGetABlog = cosmosBenchmarkRunner.BenchmarkGettingBlog(referenceId, embeddedId);
                 Console.WriteLine("Running Benchmark for Getting a Blog with all comments...");
                 var benchmarkGetBlogWithComments = cosmosBenchmarkRunner.BenchmarkGettingBlogWithAllComments(referenceId, embeddedId).GetAwaiter().GetResult();
-                csvGenerator.GenerateBenchmarkCSV(benchmarkGetBlogWithComments, blogSize, commentSize);
-
                 Console.WriteLine("Running Benchmark for Getting a Blog with some comments...");
                 var benchmarkGetBlogWithSome = cosmosBenchmarkRunner.BenchmarkGettingBlogWithSomeComments(referenceId, embeddedId, 10).GetAwaiter().GetResult();
-                csvGenerator.GenerateBenchmarkCSV(benchmarkGetBlogWithSome, blogSize, commentSize);
-
                 Console.WriteLine("Running Benchmark for Updating Comments in a blog...");
                 var benchmarkUpdatingComments = cosmosBenchmarkRunner.BenchmarkUpdatingComments(referenceId, embeddedId, 5).GetAwaiter().GetResult();
-                csvGenerator.GenerateBenchmarkCSV(benchmarkUpdatingComments, blogSize, commentSize);
-
-                string fileName = @"E:\Benchmark.csv";
-                csvGenerator.CreateFileIfNotExists(fileName);
-                csvGenerator.WriteRecords(fileName);
                 Console.WriteLine("Benchmarking has been done successfully");
             }
             catch(CosmosException e)
             {
-                Console.WriteLine("Running Benchmark has been failed with cosmos exception");
+                Console.WriteLine("Running Benchmark has been failed with some exception");
             }
             catch(Exception e)
             {
